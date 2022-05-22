2e06905319e218a81501984583948bea261d20ef
beatrizsegurap <80656281+beatrizsegurap@users.noreply.github.com>
2022-05-22 17:57:02 -0400

    nav y footer (incompletos aun)

--- Psiconline/src/app/app-routing.module.ts
+++ Psiconline/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeScreenComponentComponent } from './screen/home-screen-component/home-screen-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component:HomeScreenComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
