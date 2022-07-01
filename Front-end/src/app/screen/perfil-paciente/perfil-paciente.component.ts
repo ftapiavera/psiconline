bde87477324962987f443732b05bc254a5facb1b
ftapiavera <felipe.tapia.v@mail.pucv.cl>
2022-07-01 00:34:58 -0400

    Actualiza Componente Perfil Paciente

--- Front-end/src/app/screen/perfil-paciente/perfil-paciente.component.ts
+++ Front-end/src/app/screen/perfil-paciente/perfil-paciente.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-perfil-paciente',
   templateUrl: './perfil-paciente.component.html',
-  styleUrls: ['./perfil-paciente.component.css']
+  styleUrls: ['./perfil-paciente.component.sass']
 })
 export class PerfilPacienteComponent implements OnInit {
 
