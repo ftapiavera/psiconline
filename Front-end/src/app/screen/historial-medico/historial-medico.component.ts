643ff667aa84ff9385540d3062f547d9a7fcff27
ftapiavera <felipe.tapia.v@mail.pucv.cl>
2022-07-03 15:58:35 -0400

    CSS -> SASS

--- Front-end/src/app/screen/historial-medico/historial-medico.component.ts
+++ Front-end/src/app/screen/historial-medico/historial-medico.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-historial-medico',
   templateUrl: './historial-medico.component.html',
-  styleUrls: ['./historial-medico.component.css']
+  styleUrls: ['./historial-medico.component.sass']
 })
 export class HistorialMedicoComponent implements OnInit {
 
