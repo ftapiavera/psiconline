50b878f67cdf3a7be6be25518e319abcd822e262
ftapiavera <felipe.tapia.v@mail.pucv.cl>
2022-07-02 23:34:05 -0400

    CSS -> SASS

--- Front-end/src/app/components/nav-dashboard-admin/nav-dashboard-admin.component.ts
+++ Front-end/src/app/components/nav-dashboard-admin/nav-dashboard-admin.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-dashboard-admin',
   templateUrl: './nav-dashboard-admin.component.html',
-  styleUrls: ['./nav-dashboard-admin.component.css']
+  styleUrls: ['./nav-dashboard-admin.component.sass']
 })
 export class NavDashboardAdminComponent implements OnInit {
 
--- Front-end/src/app/components/nav-dashboard-paciente/nav-dashboard-paciente.component.ts
+++ Front-end/src/app/components/nav-dashboard-paciente/nav-dashboard-paciente.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-dashboard-paciente',
   templateUrl: './nav-dashboard-paciente.component.html',
-  styleUrls: ['./nav-dashboard-paciente.component.css']
+  styleUrls: ['./nav-dashboard-paciente.component.sass']
 })
 export class NavDashboardPacienteComponent implements OnInit {
 
